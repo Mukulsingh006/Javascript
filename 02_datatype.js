f106d8350619c0772b54bc2e44aba899c8cc09b2
Mukul  Singh <ms0897493@gmail.com>
2024-06-24 16:58:34 +0000

    container config done

---
+++ 02_datatype.js
@@ -0,0 +1,22 @@
+"use strict"// Treat all JS as newer version
+
+// alert(3+3)// this is only aplicable in Browser not in JavaScript
+
+console.log("Hello")  // code readability should be high
+
+console.log("this is new");
+
+let name = "Mukul"
+let age = "19"
+let isLoggedIn = "False"
+
+// number=> 2 to the power 53
+// Bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object 
