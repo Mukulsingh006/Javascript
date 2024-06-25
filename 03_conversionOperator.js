db8e1fe1cd3aa68e922ea165a1e8a05a2e5970ea
Mukul  Singh <ms0897493@gmail.com>
2024-06-25 12:07:33 +0000

    container config done

---
+++ 03_conversionOperator.js
@@ -0,0 +1,54 @@
+// let score = "33"
+
+// console.log(typeof score);
+
+// let valueInNumber =  Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // NAN = NOt A Number
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1 ; false => 0
+
+// let isLoggedIN = 1
+
+// let booleanIsloggedIn = Boolean(isLoggedIN)
+// console.log(booleanIsloggedIn);
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *************************************{  OPERATIONS  }**************************************************************
+
+// let value = 3
+// let negvalue = -value
+
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**3);// power 
+// console.log(2%2);
+
+// let str1 = "Hello"
+// let str2 = " Mukulsingh"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+console.log(+true);
+console.log(+"");
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2+2
+
+let gamecounter =  100
+gamecounter++;
+console.log(gamecounter);
