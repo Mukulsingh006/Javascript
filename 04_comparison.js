dae2563624df131a3995d2c15215919144ab0113
Mukul  Singh <ms0897493@gmail.com>
2024-06-27 18:14:41 +0000

    container config done

---
+++ 04_comparison.js
@@ -0,0 +1,17 @@
+// BASIC COMPARISON (Number to Number comparison)
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 > 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+// it's value found only  in true or false 
+
+
+// ADVANCE CAMPARISON
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null >= 0);// this case is special so  avoid this type of comparision
+console.log(null < 0);
