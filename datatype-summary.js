13320f78560fbc9e692b62237db0a40786cd41a2
Mukul  Singh <ms0897493@gmail.com>
2024-07-03 08:30:49 +0000

    container config done

---
+++ datatype-summary.js
@@ -0,0 +1,62 @@
+// premitive Data type
+
+// 7 types : number, null, string, boolean, Bigint,  undefined, symbol
+
+// the gives values as copy format 
+
+const score  = 100
+const scoreValue =  100.3
+
+const isLoggedIn = false 
+const outsideTemp =  null
+
+let userEmail;
+ 
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 12234533323256687n
+
+
+
+// Refrence (Non-premitive)  Data type
+
+// Array ,  Object , Function
+
+const heros = ["shaktimaan" , "Doga", "Naagraj"];
+
+let myObj = {
+    name: "Mukul",
+    age: 19,
+}
+
+const myFunction =  function(){
+
+    console.log("Hello");
+}
+
+console.log(typeof false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
