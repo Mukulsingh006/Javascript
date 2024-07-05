8f6622f461582da34c03f607b22d0a01f60b3b1e
Mukul  Singh <ms0897493@gmail.com>
2024-07-05 18:28:20 +0000

    conrtol connfig done

---
+++ 05_string.js
@@ -0,0 +1,11 @@
+const name = "mukul"
+const age = 19
+
+// console.log(name + age + "value"); // this is outdated so preffer not to  use this because it is not readable 
+
+console.log(`Hello my name is ${name } and my age is ${age}`); // it is better way to read 
+
+const thisName = new string('Hello')
+
+console.log(thisName[0]);
+console.log(thisName.__proto__);
