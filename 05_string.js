b6073570def45c8c81a921d1d5ab3a3fe2fe6ae8
Mukul  Singh <ms0897493@gmail.com>
2024-07-07 05:53:13 +0000

    control config done

--- 05_string.js
+++ 05_string.js
@@ -5,7 +5,30 @@ const age = 19
 
 console.log(`Hello my name is ${name } and my age is ${age}`); // it is better way to read 
 
-const thisName = new string('Hello')
+const random = "Hello this is a string "
 
-console.log(thisName[0]);
-console.log(thisName.__proto__);
+console.log(random[0]);
+console.log(random.__proto__);
+
+console.log(random.length);
+// console.log(random.toUpperCase);
+
+console.log(random.charAt(3));
+console.log(random.indexOf('o'));
+
+const newString = random.substring(0,5) // start counnt at zero nut last digit is not countable  ++++++
+console.log(newString)
+
+const anotherString = random.slice(-2,5)
+console.log(anotherString);
+
+const newStringOne =  "   Mukul    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://mukul.com/mukul%20singh"
+console.log(url.replace('%20', '_'));
+
+console.log(url.includes('b'));
+
+console.log(random.blink());
