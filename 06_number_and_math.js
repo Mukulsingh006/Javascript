4325c24b242c9dd221b858274e2576c15c07e946
Mukul  Singh <ms0897493@gmail.com>
2024-07-10 10:49:57 +0000

    control config done

--- 06_number_and_math.js
+++ 06_number_and_math.js
@@ -1,24 +1,37 @@
 const score =  400
-console.log(score);
+// console.log(score);
 
-const balance = new Number(100.5)
-console.log(balance);
+const balance = new Number(100.5) // this is way to express number or other things 
+// console.log(balance);
 
-console.log(balance.toString().length)
-console.log(balance.toFixed(2))
+// console.log(balance.toString().length)// it is converted into string andd ger property of string like : concat , length 
+//  console.log(balance.toFixed(2)) // it used to show decimal " () " in the colon we set the value 
 
-const otherNumber =  123.866
-console.log(otherNumber.toPrecision(4));
+const otherNumber =  123.84444
+// console.log(otherNumber.toPrecision(4));
 
 const hundred = 1000000
-console.log(hundred.toLocaleString('en-in'));
+// console.log(hundred.toLocaleString('en-in'));
 
-// +++++++++++++++++++++++++++++++++++++++++   -----   MATHS  -----        ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +++++++++++++++++++++++++++++++++++++++++   -----   MATHS  -----        +++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.round(4.9));
+// console.log(Math);
+// console.log(Math.abs(-4));
 
+// console.log(Math.round(4.9)); //  Round mean round off -if want to chose higher value so use "ceil" and for lower value "floor"
+
+// console.log(Math.min(3,2,5,7,8,9,7));
+
+// console.log(Math.max(3,2,5,7,8,9,7));
+
+console.log(Math.random());
+
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20 
+
+console.log(Math.floor(Math.random() * (max - min + 1 )) + min)
 
 
 
