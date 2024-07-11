3f78782c7854d8bc937cbcfd45e856322618de99
Mukul  Singh <ms0897493@gmail.com>
2024-07-11 13:22:31 +0000

    coontrol config done

---
+++ 07_date_and_time.js
@@ -0,0 +1,24 @@
+// 
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+
+
+
+let myCreatedDate = new Date(2024,0,27)
+console.log(myCreatedDate.toDateString());
+
+
+
+
+
+
+
+
+
+
+
+
+
