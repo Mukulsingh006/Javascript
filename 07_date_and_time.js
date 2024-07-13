7261a92e9bb413c1e9c0a7ad0baba71f69254303
Mukul  Singh <ms0897493@gmail.com>
2024-07-13 09:40:01 +0000

    container config done

--- 07_date_and_time.js
+++ 07_date_and_time.js
@@ -1,4 +1,4 @@
-// 
+// Dates And Times  //
 let myDate = new Date()
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
@@ -10,7 +10,6 @@ let myDate = new Date()
 let thisDate = new Date(2023, 0, 27)
 let hisDate = new Date(2023, 0, 27, 5, 3)
 let createdDate = new Date("2023-01-27")
-
 // console.log(myCreatedDate.toLocaleString());
 
 
@@ -18,15 +17,21 @@ let createdDate = new Date("2023-01-27")
 // console.log(myCreatedDate.toLocaleString()); // localstring is used for date time like used in office , hotels  andn others 
 
 let myTimeStamp = Date.now()
-
 // console.log(myTimeStamp);
 // console.log( myTimeStamp . getTime());
 // console.log(Math.floor(Date.now()/1000)); // this is sed for converting into second and round off the decimal number
 
+let newDate = new Date()
+// console.log(newDate.getDate)
+// console.log(newDate.getMonth() + 1)
+// console.log(newDate.getDay())
 
+// `${newDate.getDay()} and the time`
 
-
-
+newDate.toLocaleString('default', {
+   weekday:"long",
+   
+})
 
 
 
