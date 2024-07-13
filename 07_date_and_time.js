297c3a746fc7dd2b36aec439314c5dfff8dbb99d
Mukul  Singh <ms0897493@gmail.com>
2024-07-13 08:43:05 +0000

    container config done

--- 07_date_and_time.js
+++ 07_date_and_time.js
@@ -1,18 +1,27 @@
 // 
 let myDate = new Date()
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toISOString());
-console.log(myDate.toJSON());
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(typeof myDate);
 
 
+let thisDate = new Date(2023, 0, 27)
+let hisDate = new Date(2023, 0, 27, 5, 3)
+let createdDate = new Date("2023-01-27")
 
-let myCreatedDate = new Date(2024,0,27)
-console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
 
 
+//  let hisDate = new Date("01-14-2024")
+// console.log(myCreatedDate.toLocaleString()); // localstring is used for date time like used in office , hotels  andn others 
 
+let myTimeStamp = Date.now()
 
+// console.log(myTimeStamp);
+// console.log( myTimeStamp . getTime());
+// console.log(Math.floor(Date.now()/1000)); // this is sed for converting into second and round off the decimal number
 
 
 
