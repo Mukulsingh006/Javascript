bf57d88125fb53b4e51890c7aaac8dcbbaa863e5
Mukul  Singh <ms0897493@gmail.com>
2024-07-17 07:56:16 +0000

    container config done

---
+++ 02_basics.js/01_array.js
@@ -0,0 +1,28 @@
+//  ARRAY // 
+
+const myArr = [0,1,2,3,4,5]
+const myHero = ["shaktimaan", "Doga", "HULK"]
+
+const myArr2 = new Array(9,8,7,6,5)
+// console.log(myArr[2]);
+
+/////////////////////////---- ARRAY METHOD -----/////////////////////////////
+
+// myArr.push(6) // this is for add something in the end of  array 
+// myArr.pop() // it is remove last value in array
+
+// myArr.unshift(0) //  it is use for add in the start but it may create problem like change all values in array  
+// myArr.shiift()
+
+// console.log(myArr.includes(4)); // for check value
+
+const newArr = myArr.join() // converted into string and bind
+
+console.log(myArr);
+console.log(typeof newArr);
+
+
+
+
+
+
