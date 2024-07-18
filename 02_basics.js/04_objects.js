fa4a08f69212445f8edb7005707c65ad216f4da1
Mukul  Singh <ms0897493@gmail.com>
2024-07-18 17:49:25 +0000

    conatiner config done

---
+++ 02_basics.js/04_objects.js
@@ -0,0 +1,31 @@
+// const hitUser = new Object() // singleton object 
+const hitUser = {} // non singleton object
+
+hitUser.id = "123abc"
+hitUser.name = "Victor"
+hitUser.isLoggedIn = false
+
+// console.log(hitUser);
+
+const regularUser = {
+    email: "any@google.com",
+   fullname: {
+   userfullname:{
+    firstname:"mukul",
+    lastname:"singh"
+        }
+     }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a" , 2: "b"}
+const obj2 = {3: "c" , 4: "d"}
+
+// const obj3 = {obj1,obj2} this is wrong method
+// const obj3 = Object.assign({}, obj1,obj2) 
+
+const obj3 = {...obj1,...obj2}
+console.log(obj3);
+
+
